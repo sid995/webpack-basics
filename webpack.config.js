1311a3121c3a97723491a771c90f0dc435621506
Siddharth Kundu <sk31395@gmail.com>
2019-01-27 00:58:36 +0530

    changed loader image linit

--- webpack.config.js
+++ webpack.config.js
@@ -49,7 +49,7 @@ module.exports = {
       },
       {
         test: /\.(png|jpe?g|gif)/,
-        loader: 'url-loader?limit=8192000&name=images/[name].[ext]'
+        loader: 'url-loader?limit=8000&name=images/[name].[ext]'
       }
     ]
   },
